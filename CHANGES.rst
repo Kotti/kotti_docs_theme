ee6a5baf5ca61df17749b31d5aa0112a27aa72b3
Andreas kaiser <disko@binary-punks.com>
2013-01-23 12:24:49 +0100

    Make definitions of classes, functions, methods and attributes larger.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,11 @@
 Change History
 ==============
 
+0.1.1
+-----
+
+-   Make definitions of classes, functions, methods and attributes larger.
+
 0.1
 ---
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 name = 'kotti_docs_theme'
-version = '0.1'
+version = '0.1.1'
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
